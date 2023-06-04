87419cbc258f016b8a24a0e0a64b2763c75fb4ff
Arturo Jimenez <arturo.jimenez@freejolitos.com>
2023-06-04 15:49:13 -0600

    Personalizar ligas bien

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://github.com/marturojt/nextjs-notion'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
